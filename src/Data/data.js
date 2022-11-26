1c86f3bda52e470434e267baf8142acc86215ee8
rudalson <rudalson@gmail.com>
2022-11-26 16:56:03 +0900

    ✨ added: right side component

--- src/Data/data.js
+++ src/Data/data.js
@@ -5,11 +5,15 @@ import {
   UilUsersAlt,
   UilPackage,
   UilChart,
-  UilSignOutAlt,
 } from '@iconscout/react-unicons';
 
 // Analytics Cards imports
 import { UilUsdSquare, UilMoneyWithdrawal } from '@iconscout/react-unicons';
+
+// Recent Card Imports
+import img1 from '../imgs/img1.png';
+import img2 from '../imgs/img2.png';
+import img3 from '../imgs/img3.png';
 
 // Sidebar Data
 export const SidebarData = [
@@ -85,5 +89,27 @@ export const cardsData = [
         data: [10, 25, 15, 30, 12, 15, 20],
       },
     ],
+  },
+];
+
+// Recent Update Card Data
+export const UpdatesData = [
+  {
+    img: img1,
+    name: 'Andrew Thomas',
+    noti: 'has ordered Apple smart watch 2500mh battery.',
+    time: '25 seconds ago',
+  },
+  {
+    img: img2,
+    name: 'James Bond',
+    noti: 'has received Samsung gadget for charging battery.',
+    time: '30 minutes ago',
+  },
+  {
+    img: img3,
+    name: 'Iron Man',
+    noti: 'has ordered Apple smart watch, samsung Gear 2500mh battery.',
+    time: '2 hours ago',
   },
 ];
